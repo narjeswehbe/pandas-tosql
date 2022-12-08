a00806d775b0fc15a128cb01a1bf5f9655f210e8
narjeswehbe <narjeswehbi04@gmail.com>
2022-12-08 15:26:58 +0200

    first version

--- main.py
+++ main.py
@@ -9,30 +9,6 @@ from mysql.connector import Error
 from DatabaseInfo import DatabaseInfo
 
 
-def connect(connection_string):
-    global connection, cursor
-    try:
-        connection = mysql.connector.connect(host='localhost',
-                                             database='pandas',
-                                             user='root',
-                                             password='12345678',
-                                             port=3309)
-        if connection.is_connected():
-            db_Info = connection.get_server_info()
-            print("Connected to MySQL Server version ", db_Info)
-            cursor = connection.cursor()
-            cursor.execute("select database();")
-            record = cursor.fetchone()
-            print("You're connected to database: ", record)
-
-    except Error as e:
-        print("Error while connecting to MySQL", e)
-    # finally:
-    #     if connection.is_connected():
-    #         cursor.close()
-    #         connection.close()
-    #         print("MySQL connection is closed")
-
 def createFrame():
     np.random.seed(0)
     number_of_samples = 10
